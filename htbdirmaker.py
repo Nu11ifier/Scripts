a2d8f268f215fc1f71eeb74df3b8c730695780d5
Nu11ifier <85443537+Nu11ifier@users.noreply.github.com>
2023-01-16 06:02:09 +0100

    Update htbdirmaker.py

--- htbdirmaker.py
+++ htbdirmaker.py
@@ -7,22 +7,11 @@ import subprocess
 
 def main():
     try:
-        if not os.path.exists("/usr/local/bin/htbdirmaker.py"):
-            path = os.getcwd()
-            print(path)
-            subprocess.run(
-                "sudo ln -s "+path+"/htbdirmaker.py /usr/local/bin/htbdirmaker.py", shell=True) #change 'shell=True' later because of shell injection.
-            subprocess.run("sudo chmod +x /usr/local/bin/htbdirmaker.py", shell=True)
-            print("Link Created - Use Command Anywhere.")
-    except:
-        print("Could Not Create Link.")
-        exit()
-    try:
         option = sys.argv[1]
     except:
         print("Error: Wrong format")
         print("Usage: htbdirmaker.py -[Option]")
-        exit()
+        sys.exit()
 
     if option == "-h":
         _h()
@@ -42,34 +31,39 @@ def _h():
 def _d():
     try:
         parent_dir = sys.argv[2]
-        os.mkdir(parent_dir, mode=0o666)
-        print("Directory '% s' is built!" % parent_dir)
-        print()
+        path = os.getcwd()
+        if not os.path.exists(path + "/" + parent_dir):
+            os.mkdir(parent_dir, mode=0o700)
+            print("Directory '% s' is built!" % parent_dir)
+            print()
+        else:
+            print("Specified Directory Already Exists!")
+            return
     except:
         print("Error: Specify a valid directory path!")
         print()
         print("Usage: htbdirmaker.py -d [directory name]")
-        exit()
+        sys.exit()
 
     try:
         sub_dir_nmap = os.path.join(parent_dir, "nmap")
         sub_dir_scripts = os.path.join(parent_dir, "scripts")
         sub_dir_notes = os.path.join(parent_dir, "notes")
 
-        os.mkdir(sub_dir_nmap, mode=0o666)
+        os.mkdir(sub_dir_nmap, mode=0o700)
         print("Directory '% s' is built!" % sub_dir_nmap)
         print()
 
-        os.mkdir(sub_dir_scripts, mode=0o666)
+        os.mkdir(sub_dir_scripts, mode=0o700)
         print("Directory '% s' is built!" % sub_dir_scripts)
         print()
 
-        os.mkdir(sub_dir_notes, mode=0o666)
+        os.mkdir(sub_dir_notes, mode=0o700)
         print("Directory '% s' is built!" % sub_dir_notes)
         print()
     except:
         print("Error: Failed to build sub directories!")
-        exit()
+        sys.exit()
 
 
 main()
